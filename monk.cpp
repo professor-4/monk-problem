bdc8f0e71027100fc713547bb6485b017a3d6cce
professor-4 <akashkushwah1607@gmail.com>
2020-03-05 14:13:08 +0530

    my project

---
+++ monk.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int n;cin>>n;
+	int arr1[n],arr2[n],arr3[n];
+	for (int i=0;i<n;i++){
+		cin>>arr1[i];
+	}
+	for (int i=0;i<n;i++){
+		cin>>arr2[i];
+	}
+	for (int i=0;i<n;i++){
+		arr3[i]=arr1[i]+arr2[i];
+	}
+	for (int i=0;i<n;i++){
+		cout<<arr3[i]<<" ";
+	}
+}
